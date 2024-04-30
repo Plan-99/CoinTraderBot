7b1375e08b85783aaa35b2dec70acfc8a2c7a877
정해찬 <>
2024-04-30 15:05:51 +0900

    no message

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-nohup $0 ./get_price.sh &
+sudo nohup $0 ./get_price.sh &
 
 cd python
-nohup python3 player1.py &
+sudo nohup python3 player1.py &
 echo "Player 1 start"
-nohup python3 player2.py &
+sudo nohup python3 player2.py &
 echo "Player 2 start"
 cd ../
