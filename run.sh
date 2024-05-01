9160795522068b1ce95df57029cab144dd6567c2
정해찬 <>
2024-05-01 12:42:29 +0900

    no message

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 sudo nohup $1 ./get_price.sh &
 
-cd /var/www/CoinTraderBot/python
+cd python
 nohup python3 player1.py &
 echo "Player 1 start"
-cd ~
 sudo mv nohup.out nohup1.out
-cd /var/www/CoinTraderBot/python
 nohup python3 player2.py &
 echo "Player 2 start"
-cd ~
 sudo mv nohup.out nohup2.out
 cd ../
